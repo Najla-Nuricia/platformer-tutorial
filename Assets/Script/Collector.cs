51db61c3eb2251b320c5edf1b6829fcb388ad42c
NAJLA NURICIA LAUDY <najlalaudy@gmail.com>
2026-01-07 11:29:22 +0700

    fix project

--- Assets/Script/Collector.cs
+++ Assets/Script/Collector.cs
@@ -1,6 +1,8 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class Collector : MonoBehaviour
 {
